8880713a1e0813097229e0574ad246e3beb1e17a
Helio Dourado <heliohdd@yahoo.com.br>
2021-05-03 17:21:11 -0300

    Project created

---
+++ backend/src/main/java/com/hdtec/hdvendas/HdvendasApplication.java
@@ -0,0 +1,13 @@
+package com.hdtec.hdvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hdtec/hdvendas/HdvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdtec.hdvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
