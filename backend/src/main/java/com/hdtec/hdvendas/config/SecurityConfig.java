b8b2a7ceb9f6eb2b67782f34b6a0392fdc3da573
Helio Dourado <heliohdd@yahoo.com.br>
2021-05-05 16:14:28 -0300

    Domain model, database seed

--- backend/src/main/java/com/hdtec/hdvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/hdtec/hdvendas/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 	@Autowired
 	private Environment env;
 
