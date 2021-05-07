cd9adb0f4bf0703aaac80d95cc4b15a3c81f77a0
Helio Dourado <heliohdd@yahoo.com.br>
2021-05-07 14:03:35 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,27 @@
+import Footer from "components/Footer";
+import NavBar from "components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home
- = () => {
-    return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
-    );
-}
+    = () => {
+        return (
+            <>
+                <NavBar />
+                <div className="container">
+                    <div className="jumbotron">
+                        <h1 className="display-4">HDVendas</h1>
+                        <p className="lead">Analise o desempenho das suas vendas em diferentes perspectivas</p>
+                        <hr />
+                        <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                        <Link className="btn btn-primary btn-lg" to="/dashboard">
+                            Acessar Dashboard
+                        </Link>
+                    </div>
+                </div>
+                <Footer />
+            </>
+        );
+    }
 
 export default Home
-;
+    ;
